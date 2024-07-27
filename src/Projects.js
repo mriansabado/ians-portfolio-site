fc451816c259d02d6edc9d0835f5ad2fc3e586be
Ian <isabado@onpointglobal.com>
2024-07-26 17:15:52 -0700

    added the 3 projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,12 +1,13 @@
 import React from "react";
 import BarblendImage from './assets/barblend.png';
 import Fontastic from './assets/fontastic-home.jpeg';
+import JSDrumkit from './assets/JS-Drumkit.png';
 
 const projects = [
   { title: 'Fontastic', description: 'Input text and have it appear on your screen in full size. Communicate non-verbally to your friends across the room! Stack: React Native | Lottie files', link: '#', image: Fontastic },
   { title: 'BarBlend Guru', description: 'Lookup cocktail recipes and get drink suggestions by utilizing this web app. Stack: Next JS | Tailwind CSS | Rest API', link: '#', image: BarblendImage },
-  { title: 'Project 3', description: 'Description for project 3', link: '#', image: 'https://via.placeholder.com/400' },
-  { title: 'Project 4', description: 'Description for project 4', link: '#', image: 'https://via.placeholder.com/400' },
+  { title: 'Javascript Drumkit', description: 'Use your computer keyboard as a drumkit', link: 'https://mriansabado.github.io/javaScript-drumkit/', image: JSDrumkit },
+  { title: 'Project 4', description: 'Description for project 4', link: 'https://mriansabado.github.io/javaScript-drumkit/', image: 'https://via.placeholder.com/400' },
 ];
 
 const ProjectCard = ({ title, description, link, image }) => (
@@ -16,7 +17,7 @@ const ProjectCard = ({ title, description, link, image }) => (
     <div className="absolute inset-0 bg-white bg-opacity-90 p-6 flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300">
       <h2 className="text-2xl font-bold mb-2 text-gray-900">{title}</h2>
       <p className="text-gray-700 mb-4">{description}</p>
-      <a href={link} className="mt-4 text-blue-500 hover:text-blue-700 transition-colors duration-300">Check it out</a>
+      <a href={link} target="_blank" rel="noopener noreferrer" className="mt-4 text-blue-500 hover:text-blue-700 transition-colors duration-300">Check it out</a>
     </div>
   </div>
 );
