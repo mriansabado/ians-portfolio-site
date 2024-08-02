350dd6ebd2b3dd8eef37f923a88865307a9d0a8d
Ian <isabado@onpointglobal.com>
2024-08-01 22:00:17 -0700

    removed blank project | updated cards to show stack details

--- src/Projects.js
+++ src/Projects.js
@@ -4,19 +4,25 @@ import Fontastic from './assets/fontastic-image.png';
 import JSDrumkit from './assets/JS-Drumkit.png';
 
 const projects = [
-  { title: 'Fontastic', description: 'Input text and have it appear on your screen in full size. Communicate non-verbally to your friends across the room! Stack: React Native | Lottie files', link: '#', image: Fontastic },
-  { title: 'BarBlend Guru', description: 'Lookup cocktail recipes and get drink suggestions by utilizing this web app. Stack: Next JS | Tailwind CSS | Rest API', link: 'https://main.d2e1ldx3lbiju.amplifyapp.com/', image: BarblendImage },
-  { title: 'Javascript Drumkit', description: 'Use your computer keyboard as a drumkit', link: 'https://mriansabado.github.io/javaScript-drumkit/', image: JSDrumkit },
-  { title: 'Project 4', description: 'Description for project 4', link: '#', image: 'https://via.placeholder.com/400' },
+  {
+    title: 'Fontastic', description: 'Input text and have it appear on your screen in full size. Communicate non-verbally to your friends across the room!', stack: 'React Native | Tailwind CSS | Expo | Lottie files', link: '#', image: Fontastic
+  },
+  {
+    title: 'BarBlend Guru', description: 'Lookup cocktail recipes and get drink suggestions by utilizing this web app.', stack: 'React | Next JS | Tailwind CSS | Rest API | AWS Cloud Services', link: 'https://main.d2e1ldx3lbiju.amplifyapp.com/', image: BarblendImage
+  },
+  {
+    title: 'Javascript Drumkit', description: 'Use your computer keyboard as a drumkit', stack: 'JavaScript, HTML, CSS', link: 'https://mriansabado.github.io/javaScript-drumkit/', image: JSDrumkit
+  }
 ];
 
-const ProjectCard = ({ title, description, link, image }) => (
+const ProjectCard = ({ title, description, stack, link, image }) => (
   <div className="relative p-6 bg-white bg-opacity-80 backdrop-filter backdrop-blur-lg shadow-md hover:shadow-lg transition-shadow duration-300 rounded-lg transform hover:scale-105 transition-transform duration-300 overflow-hidden">
     <img src={image} alt={title} className="w-full h-auto object-cover rounded-md mb-4" style={{ width: '400px', height: '400px' }} />
     <h2 className="text-2xl font-bold text-gray-900">{title}</h2>
     <div className="absolute inset-0 bg-white bg-opacity-90 p-6 flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300">
       <h2 className="text-2xl font-bold mb-2 text-gray-900">{title}</h2>
       <p className="text-gray-700 mb-4">{description}</p>
+      <p className="text-gray-700 mb-4">{stack}</p>
       <a href={link} target="_blank" rel="noopener noreferrer" className="mt-4 text-blue-500 hover:text-blue-700 transition-colors duration-300">Check it out</a>
     </div>
   </div>
@@ -31,7 +37,8 @@ export default function Projects() {
           <ProjectCard 
             key={index} 
             title={project.title} 
-            description={project.description} 
+            description={project.description}
+            stack={project.stack}
             link={project.link} 
             image={project.image} 
           />
