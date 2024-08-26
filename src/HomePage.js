b5fd5591b7f8d3392852fef0945ecb692b0cd13b
mriansabado <mriansabado@gmail.com>
2024-08-25 22:36:28 -0700

    fixed the hero section background

--- src/HomePage.js
+++ src/HomePage.js
@@ -13,19 +13,21 @@ const HomePage = () => {
         className="main-wrap relative z-10 min-h-screen" 
         style={{ 
           backgroundImage: `url(${NightImage})`, 
-          backgroundRepeat: "no-repeat", // Prevents the image from repeating
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover", // Cover the entire area
+          backgroundPosition: "center", // Center the image
+          backgroundAttachment: "fixed", // Keeps the background fixed on desktop
         }}
       >
 
         {/* Hero Section */}
-        <div className="hero min-h-screen relative bg-transparent">
-          <div className="absolute inset-0"></div>
-          <div className="hero-content text-center text-neutral-content relative z-10 flex items-center justify-center px-4 md:px-0">
-            <div className="relative w-full max-w-3xl bg-opacity-75 p-8 md:p-20 rounded-lg shadow-2xl overflow-hidden">
-              <div className="relative z-10">
-                <h1 className="mb-5 text-5xl md:text-6xl font-bold text-white libre-baskerville-bold">Ian Sabado</h1>
-                <p className="mb-5 text-2xl md:text-3xl text-white libre-baskerville-regular">Software Engineer <br /> San Francisco Bay Area</p>
-              </div>
+        <div className="hero min-h-screen relative bg-transparent flex items-center justify-center">
+          <div className="relative w-full max-w-3xl bg-opacity-75 p-8 md:p-20 rounded-lg shadow-2xl overflow-hidden">
+            <div className="relative z-10 text-center text-neutral-content">
+              <h1 className="mb-5 text-5xl md:text-6xl font-bold text-white libre-baskerville-bold">Ian Sabado</h1>
+              <p className="mb-5 text-2xl md:text-3xl text-white libre-baskerville-regular">
+                Software Engineer <br /> San Francisco Bay Area
+              </p>
             </div>
           </div>
         </div>
@@ -43,7 +45,21 @@ const HomePage = () => {
       <style jsx>{`
         @media (max-width: 768px) {
           .main-wrap {
-            background-position: top; /* Different perspective on mobile */
+            background-attachment: scroll; /* Allow the background to scroll with content on mobile */
+            background-position: center top; /* Adjust the position for mobile */
+            background-size: cover; /* Ensure the background still covers the area */
+          }
+
+          .hero-content {
+            padding: 16px; /* Adjust padding for smaller screens */
+          }
+
+          h1 {
+            font-size: 2.5rem; /* Adjust font size for smaller screens */
+          }
+
+          p {
+            font-size: 1.25rem; /* Adjust font size for smaller screens */
           }
         }
       `}</style>
